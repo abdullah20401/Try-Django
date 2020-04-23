7335dbc08c61735ef47d4d902337b572001fcc02
Abdullah Siddiqui <44303300+abdullah20401@users.noreply.github.com>
2020-04-23 14:30:31 -0500

    Video 23 TryDjango Series

--- src/products/forms.py
+++ src/products/forms.py
@@ -4,3 +4,8 @@ from .models import Product
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
+        fields = [
+            'title',
+            'description',
+            'price'
+        ]
